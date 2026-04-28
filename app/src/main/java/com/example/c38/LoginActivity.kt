c08faad0adb61347fb6dba1a11ff9b645a46fd9e
Sprajapati123 <sandisprajapati52@gmail.com>
2026-04-28 08:31:48 +0545

    login

--- app/src/main/java/com/example/c38/LoginActivity.kt
+++ app/src/main/java/com/example/c38/LoginActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +34,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.c38.ui.theme.Blue
 import com.example.c38.ui.theme.C38Theme
+import com.example.c38.ui.theme.Purple40
+import com.example.c38.ui.theme.PurpleGrey40
 import com.example.c38.ui.theme.SmokyWhite
 
 class LoginActivity : ComponentActivity() {
@@ -77,12 +82,26 @@ fun LoginBody() {
                 .padding(horizontal = 20.dp)
         ) {
             Card(
+                shape = RoundedCornerShape(12.dp),
                 modifier = Modifier
                     .height(60.dp)
-                    .weight(1f)
+                    .weight(1f),
+                colors = CardDefaults.cardColors(
+                    containerColor = PurpleGrey40
+                )
             ) {
-                Row(modifier = Modifier.fillMaxSize()) {
-
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                    ) {
+                    Image(
+                        painter = painterResource(R.drawable.faceebook),
+                        contentDescription = null,
+                        modifier = Modifier.size(30.dp)
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text("Facebook")
                 }
             }
             Spacer(modifier = Modifier.width(20.dp))
