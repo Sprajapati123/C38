72de7d2abc8649cc5ef0468667c0a50aadd5a3b2
Sprajapati123 <sandisprajapati52@gmail.com>
2026-04-30 12:37:55 +0545

    login

--- app/src/main/java/com/example/c38/LoginActivity.kt
+++ app/src/main/java/com/example/c38/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.c38
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,14 +18,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -51,6 +65,10 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginBody() {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -100,7 +118,71 @@ fun LoginBody() {
 
         }
 
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(all = 20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            HorizontalDivider(
+                modifier = Modifier.weight(1f),
+                thickness = 2.dp,
+                color = Color.Gray.copy(alpha = 0.2f)
+            )
+            Text("OR", modifier = Modifier.padding(horizontal = 15.dp))
+            HorizontalDivider(
+                modifier = Modifier.weight(1f),
+                thickness = 2.dp,
+                color = Color.Gray.copy(alpha = 0.2f)
+            )
+        }
 
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                //ram
+                email = it
+            },
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("abc@gmail.com")
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                unfocusedContainerColor = Color.Gray.copy(alpha = 0.1f),
+                focusedContainerColor = Color.Gray.copy(alpha = 0.1f),
+                focusedIndicatorColor = Color.Blue,
+            )
+        )
+
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                //ram
+                password = it
+            },
+//            leadingIcon = { Icon(Icons.Default.Email,contentDescription = null) },
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_visibility_24),
+                        contentDescription = null
+                    )
+                }
+            },
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("********")
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                unfocusedContainerColor = Color.Gray.copy(alpha = 0.1f),
+                focusedContainerColor = Color.Gray.copy(alpha = 0.1f),
+                focusedIndicatorColor = Color.Blue,
+            )
+        )
 
     }
 }
@@ -108,9 +190,9 @@ fun LoginBody() {
 @Composable
 fun LoginCard(
     modifier: Modifier,
-    image : Int,
-    label : String
-){
+    image: Int,
+    label: String
+) {
     Card(
         shape = RoundedCornerShape(12.dp),
         modifier = modifier,
