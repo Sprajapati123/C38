5ccadc6ae079478e8cc056b45ec40782fe89a08f
Sprajapati123 <sandisprajapati52@gmail.com>
2026-06-07 11:43:39 +0545

    product fetch

--- app/src/main/java/com/example/c38/HomeScreen.kt
+++ app/src/main/java/com/example/c38/HomeScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -109,6 +110,17 @@ fun HomeBody() {
                     Text(data.name)
                     Text(data.price.toString())
                     Text(data.description)
+                    TextButton(onClick = {}) {
+                        Text("Edit")
+                    }
+                    TextButton(onClick = {
+                        productViewModel.deleteProduct(data.id){
+                            success,msg->
+                        }
+                    }) {
+                        Text("Delete")
+
+                    }
                 }
             }
 
