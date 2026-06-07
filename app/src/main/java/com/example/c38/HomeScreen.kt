efbfa0974406af2d26e4c1129584d2dde6c1af5e
Sprajapati123 <sandisprajapati52@gmail.com>
2026-06-07 11:37:49 +0545

    product fetch

--- app/src/main/java/com/example/c38/HomeScreen.kt
+++ app/src/main/java/com/example/c38/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
@@ -28,12 +29,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.c38.repo.ProductRepoImpl
 import com.example.c38.ui.theme.C38Theme
+import com.example.c38.viewmodel.ProductViewModel
 
 class HomeScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,15 +55,14 @@ class HomeScreen : ComponentActivity() {
 @Composable
 fun HomeBody() {
 
-    val productList = listOf(
-        ProductModel(image = R.drawable.google, username = "Google"),
-        ProductModel(image = R.drawable.faceebook, username = "Facebook"),
-        ProductModel(image = R.drawable.apple, username = "Apple"),
-        ProductModel(image = R.drawable.samsung, username = "Samsung"),
-        ProductModel(image = R.drawable.picker, username = "Gambler"),
-        ProductModel(image = R.drawable.ball, username = "Baller"),
+    val productViewModel = remember { ProductViewModel(ProductRepoImpl()) }
 
-        )
+    LaunchedEffect(Unit) {
+        productViewModel.getAllProduct()
+    }
+
+    val allProducts = productViewModel.allProducts.observeAsState(initial = emptyList())
+
 
     Scaffold(
         topBar = {
@@ -88,214 +93,24 @@ fun HomeBody() {
 
                 )
         },
-        bottomBar = {
 
-        }
+        ) {
 
-    ) {
-
-        padding ->
+            padding ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
         ) {
-            item {
 
-                LazyRow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 20.dp)
-                ) {
-                    items(productList.size) { index ->
-                        Column(
-                            modifier = Modifier.padding(end = 20.dp)
-                        ) {
-                            Image(
-                                painter = painterResource(productList[index].image),
-                                contentDescription = null,
-                                modifier = Modifier.size(60.dp)
-                            )
-                            Text(productList[index].username)
-                        }
-                    }
+            items(allProducts.value!!.size) { index ->
+                val data = allProducts.value!![index]
+                Column {
+                    Text(data.name)
+                    Text(data.price.toString())
+                    Text(data.description)
                 }
             }
-            items(productList.size) { index ->
-                //design like post of instagram feed
-            }
-
-
-            //        item {
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text("Submit")
-            //            }
-            //        }
-            //        items(100){index->
-            //            ElevatedButton(
-            //                onClick = {},
-            //                modifier = Modifier.height(200.dp)
-            //            ) {
-            //                Text(index.toString())
-            //            }
-            //        }
-            //        item{
-            //            Text("njklshdfjbsdkdj")
-            //        }
-
 
         }
     }
